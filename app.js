da1ccbac6e2293559c599b56223aabc33479c8d2
Muhamad Nur Syami <nursami385@gmail.com>
2023-11-28 18:08:49 +0700

    tambah handle cors

--- app.js
+++ app.js
@@ -12,8 +12,24 @@ db.then(() => {
 }).catch(() => {
   console.log("gagal connect ke MonggoDB");
 });
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
 // Midlleware
-app.use(cors());
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
