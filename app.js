bd784b97ec234669e2dffde23bf166f2f7f120c6
Muhamad Nur Syami <nursami385@gmail.com>
2023-11-28 18:47:31 +0700

    menambahkan limit upload

--- app.js
+++ app.js
@@ -30,9 +30,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // ROUTES
 app.use(allRoutes);
