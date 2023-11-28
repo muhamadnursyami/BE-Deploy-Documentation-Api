4be1938f39fab37b32e672a5fee8b69e3d86a32a
Dedy Lumbantobing <dedylumbantobing3@gmail.com>
2023-11-28 20:29:55 +0700

    Berhasil configurasi midtrans

--- controllers/midtrans.controllers.js
+++ controllers/midtrans.controllers.js
@@ -2,53 +2,76 @@ const db = require("../config/db");
 const Transaksi = db.transaksi;
 const { coreApi } = require("../utils/midtrans");
 const midtransClient = require("midtrans-client");
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
+const Donation = require("../models/midtrans");
 
+// Controller untuk menangani charge transaksi menggunakan Midtrans
 exports.midtransChargeTransaction = async (req, res) => {
-  //   if (!req.body || Object.keys(req.body).length === 0) {
-  //     return res.status(400).send({
-  //       success: false,
-  //       message: "Content can not be empty!",
-  //     });
-  //   }
-
   try {
-    // lakukan charge transaksi ke server midtrans sesuai request
-    // const chargeResponse = await coreApi.charge(req.body);
+    // Mengambil data dari body request
+    const { full_name, email, phone, donation_amount, note } = req.body;
 
+    // Memastikan tidak ada field yang kosong dalam request
+    if (!full_name || !email || !phone || !donation_amount) {
+      return res.status(400).json({
+        success: false,
+        message: "Terdapat kolom yang kosong dalam permintaan.",
+      });
+    }
+
+    // Membuat objek data donasi untuk disimpan di MongoDB
+    const donationData = new Donation({
+      order_id: new ObjectId(), // Membuat ObjectId baru untuk order_id
+      full_name,
+      email,
+      phone,
+      donation_amount,
+      note,
+    });
+
+    // Menyimpan data donasi ke MongoDB
+    const savedDonation = await donationData.save();
+    console.log("Berhasil menyimpan data donasi:", savedDonation);
+
+    // Menyiapkan konfigurasi dan parameter untuk charge transaksi ke Midtrans
     let snap = new midtransClient.Snap({
-      // Set to true if you want Production Environment (accept real transaction).
       isProduction: false,
       serverKey: process.env.SERVER_KEY,
     });
 
     let parameter = {
       transaction_details: {
-        order_id: Math.floor(10000 + Math.random() * 90000),
-        gross_amount: 10000,
+        order_id: donationData.order_id.toString(), // Mengubah ObjectId menjadi string
+        gross_amount: donation_amount,
       },
       credit_card: {
         secure: true,
       },
       customer_details: {
-        first_name: "Dedy",
-        last_name: "Lumbantobing",
-        email: "dedy@example.com",
-        phone: "08111222333",
+        first_name: full_name,
+        email,
+        phone,
       },
     };
 
-    snap.createTransaction(parameter).then((transaction) => {
-      // transaction token
-      // let transactionToken = transaction.token;
-      // console.log('transactionToken:',transactionToken);
+    // Melakukan charge transaksi ke server Midtrans
+    snap.createTransaction(parameter).then(async (transaction) => {
+      // Menyimpan response_midtrans dan transaction_status ke objek donasi
+      donationData.response_midtrans = transaction.token;
+      donationData.transaction_status = transaction.transaction_status;
+
+      // Menyimpan data donasi ke MongoDB
+      const savedDonation = await donationData.save();
 
       return res.status(201).json({
         success: true,
-        message: "Berhasil melakukan charge transaction!",
+        message: "Berhasil melakukan charge transaksi!",
         data: transaction,
       });
     });
   } catch (error) {
+    // Menangani kesalahan dan memberikan respons server error
     return res.status(500).json({ success: false, message: error.message });
   }
 };
--- models/midtrans.js
+++ models/midtrans.js
@@ -1,13 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const transaksiSchema = new mongoose.Schema({
+const transactionSchema = new mongoose.Schema({
   order_id: {
+    type: String,  
+    required: true,
+  },
+  full_name: {
     type: String,
     required: true,
   },
-  nama: {
+  email: {
     type: String,
+  },
+  phone: {
+    type: String,
+  },
+  donation_amount: {
+    type: Number,
     required: true,
+  },
+  note: {
+    type: String,
   },
   response_midtrans: {
     type: String,
@@ -17,6 +30,4 @@ const transaksiSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('Transaksi', transaksiSchema);
-
-// module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Transaction", transactionSchema);
