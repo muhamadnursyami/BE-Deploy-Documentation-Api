2e27744b57e7d1420a7eba6322770933dc87327e
Dedy Lumbantobing <dedylumbantobing3@gmail.com>
2023-11-28 22:55:35 +0700

    test donasi buku

--- controllers/donasi.controllers.js
+++ controllers/donasi.controllers.js
@@ -1,6 +1,9 @@
 const Video = require("../models/video");
+const Buku = require("../models/buku");
 const Donasi = require("../models/donasi");
+const R2 = require("aws-sdk");
 const cloudinary = require("../utils/cloudinary");
+// const upload = require("../utils/multer");
 
 module.exports = {
   donasiVideo: async (req, res) => {
@@ -63,6 +66,78 @@ module.exports = {
     } catch (error) {
       console.log(error);
       res.status(500).send(error.message);
+    }
+  },
+
+  donasiBuku: async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const publicBucketUrl = process.env.R2_PUBLIC_BUCKET_URL;
+      const D2 = new R2.S3({
+        region: "auto",
+        endpoint: process.env.R2_ENDPOINT,
+        credentials: {
+          accessKeyId: process.env.R2_ACCESS_KEY_ID,
+          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+        },
+      });
+
+      // Upload book file
+      let bookKey = Math.round(Math.random() * 99999999999999).toString();
+      const bookUrl = publicBucketUrl + bookKey;
+      await D2.upload({
+        Body: req.files.book_url[0].buffer,
+        Bucket: process.env.R2_BUCKET_NAME,
+        Key: bookKey,
+        ContentType: req.files.book_url[0].mimetype,
+      }).promise();
+
+      // Upload image file
+      let imgKey = Math.round(Math.random() * 99999999999999).toString();
+      const imgUrl = publicBucketUrl + imgKey;
+      await D2.upload({
+        Body: req.files.img_url[0].buffer,
+        Bucket: process.env.R2_BUCKET_NAME,
+        Key: imgKey,
+        ContentType: req.files.img_url[0].mimetype,
+      }).promise();
+
+      let buku = new Buku({
+        title: req.body.title,
+        description: req.body.description,
+        author: req.body.author,
+        tahun_terbit: req.body.tahun_terbit,
+        rating: req.body.rating,
+        star: req.body.star,
+        img_url: imgUrl,
+        book_url: bookUrl,
+        download_url: bookUrl,
+        category: req.body.category,
+      });
+
+      const savedBuku = await buku.save();
+
+      const donasi = new Donasi({
+        bukuID: savedBuku._id,
+        userID: id,
+      });
+
+      const savedDonasi = await donasi.save();
+
+      savedBuku.donaturId = savedDonasi._id;
+      await savedBuku.save();
+
+      res.json({
+        message: "Book donation successful",
+        data: buku,
+      });
+    } catch (error) {
+      console.log(error);
+      res.json({
+        message: "Book donation failed",
+        data: error,
+      });
     }
   },
 };
--- routes/donasiRoutes.js
+++ routes/donasiRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { donasiVideo } = require("../controllers/donasi.controllers");
+const { donasiVideo, donasiBuku } = require("../controllers/donasi.controllers");
 const upload = require("../utils/multer");
 const route = express.Router();
 
 route.post("/donasivideo/:id", upload.single("file"), donasiVideo);
+route.post("/donasibuku/:id", upload.fields([{ name: 'img_url', maxCount: 1 }, { name: 'book_url', maxCount: 1 }]), (req, res) => {
+    // Memeriksa apakah file berhasil diunggah sebelum menyimpan atau memprosesnya
+    if (!req.files) {
+        return res.status(400).json({ error: "No file uploaded" });
+    }
+    donasiBuku(req, res);
+});
 
 module.exports = route;
