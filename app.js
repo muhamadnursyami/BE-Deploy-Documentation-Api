72c306ef4335c2f2fc9035d2192a8f05b246c3d4
Dedy Lumbantobing <dedylumbantobing3@gmail.com>
2023-11-30 12:06:29 +0700

    Revisi code

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db = require("./config/db");
 const allRoutes = require("./routes");
+
 // check db connection
 db.then(() => {
   console.log("berhasil connect ke MongoDB");
@@ -14,10 +16,17 @@ db.then(() => {
 });
 
 // Midlleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // replace with your client's origin
+  credentials: true,
+}));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'favicon.ico'));
+});
 
 // ROUTES
 app.use(allRoutes);
