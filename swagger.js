e4613e26a1391e95a459395aa0100e656dd28bfb
Muhamad Nur Syami <nursami385@gmail.com>
2023-12-03 20:04:34 +0700

    change url

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000", // Change this according to your server configuration
+        url: "https://fantastic-clam-tam.cyclic.app", // Change this according to your server configuration
       },
     ],
   },
